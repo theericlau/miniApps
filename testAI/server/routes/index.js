b3b63a67889a5ee90e3fcfcebc14b8405dddade6
Eric <theericlau@gmail.com>
2018-03-29 21:51:56 -0700

    rendered out some components

--- testAI/server/routes/index.js
+++ testAI/server/routes/index.js
@@ -1,10 +1,28 @@
 const router = require('express').Router();
-const coins = require('../controllers/coins');
+const app = require('../controllers/app');
+const axios = require('axios');
+// const suggestions = require('../../data/data.json');
 
 router.get('/', (req, res) => {
   res.send(200);
 });
 
-router.get('/coins', coins.get);
+router.get('/app', (req, res) => {
+  // console.log('i get in here', suggestions);
+  axios.get('http://localhost:5000/data/data.json')
+  .then(response => {
+    // console.log('response', response.data);
+    res.send(response.data);
+  })
+});
+
+router.get('/pic/:picId', (req, res)=> {
+  // console.log("req", req.params.picId);
+  // axios.get(`http://localhost:5000/data/${req.params.picId}`)
+  res.send(`http://localhost:5000/data/${req.params.picId}`)
+  // console.log('res', res);
+})
+
+
 
 module.exports = router;
