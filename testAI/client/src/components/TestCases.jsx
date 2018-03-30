f4b918b901fc5113018007579563b5bd1ccf6058
Eric <theericlau@gmail.com>
2018-03-29 21:49:38 -0700

    rendered out testcases

---
+++ testAI/client/src/components/TestCases.jsx
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+
+class TestCases extends Component {
+  render() {
+    return (
+      <div>
+        TestCase
+      </div>
+    );
+  }
+}
+
+TestCases.propTypes = {
+
+};
+
+export default TestCases;
