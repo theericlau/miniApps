92f4495bb31089e53bf5fac6ad20b21268a3659e
Eric <theericlau@gmail.com>
2018-03-30 21:24:57 -0700

    edited routes

--- testAI/server/routes/index.js
+++ testAI/server/routes/index.js
@@ -1,26 +1,29 @@
 const router = require('express').Router();
-const app = require('../controllers/app');
 const axios = require('axios');
-// const suggestions = require('../../data/data.json');
+const path = require('path');
+
 
 router.get('/', (req, res) => {
   res.send(200);
 });
 
+// router.get('/app/*', (req, res) => {
+//   res.sendFile(path.join(__dirname, '../../client/dist/index.html')), err => {
+//     if (err) {
+//       res.status(500).send(err);
+//     }
+//   }
+// })
+
 router.get('/app', (req, res) => {
-  // console.log('i get in here', suggestions);
   axios.get('http://localhost:5000/data/data.json')
   .then(response => {
-    // console.log('response', response.data);
     res.send(response.data);
   })
 });
 
 router.get('/pic/:picId', (req, res)=> {
-  // console.log("req", req.params.picId);
-  // axios.get(`http://localhost:5000/data/${req.params.picId}`)
   res.send(`http://localhost:5000/data/${req.params.picId}`)
-  // console.log('res', res);
 })
 
 
